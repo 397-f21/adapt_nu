61ba68c1f16e9212e4dc6f3e0cabdbd4bee3452e
sydneygil <sgil@u.northwestern.edu>
2021-11-02 23:34:47 -0500

    attempt test 1

--- src/search.test.js
+++ src/search.test.js
@@ -1,8 +1,13 @@
 import { render, screen, getByText} from "@testing-library/react";
 import Search from './search'
+import * as admin from 'firebase-admin';
 
 it("LocationCard renders with correct data", () => {
   render(<LocationCard name="test name" desc="test desc" />);
   expect(screen.getByText("test name")).toBeTruthy()
   expect(screen.getByText("test desc")).toBeTruthy()
+});
+
+it("should throw error if Search is called with a typo", () => {
+  //render(<Search )
 });
